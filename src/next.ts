cad17de3a5534e3f235aa63efcf03e464e838825
Víctor Morilla <Test@ecb.com>
2025-02-16 16:01:32 +0100

    Case of coincidence with transparent patter

--- src/next.ts
+++ src/next.ts
@@ -21,10 +21,19 @@ export function tilesetToNextPatterns(tileset: Tileset, colorFn: ColorFn): Buffe
     return buffer;
 }
 
-export function nextColor256(transparentIndex = 227) {
+/**
+ * Returns a funcion that maps a color to a 256 color index
+ * @param transparentIndex Index of the transparent color
+ * @param alternativeTransparentColor Color to be used if a non-transparent color matches the index of the transparent one
+ * @returns 
+ */
+export function nextColor256(transparentIndex = 227, alternativeTransparentColor = 228) {
     return (color: RGBAColor) => {
         const [r, g, b, a] = color;
-        return a === 0 ? transparentIndex : (r & 0b11100000) | ((g & 0b11100000) >> 3) | ((b & 0b11000000) >> 6);
+        if (a === 0)
+            return transparentIndex;
+        const index = (r & 0b11100000) | ((g & 0b11100000) >> 3) | ((b & 0b11000000) >> 6);
+        return index === transparentIndex ? alternativeTransparentColor : index;
     }
 }
 
